ebf8bb5cf390274b33ffa795e73d696949f1a517
tianwanshan <wanshantian@gmail.com>
2022-06-15 15:22:56 +0800

    update gRPC

--- RPC/gRPC/Client/main.go
+++ RPC/gRPC/Client/main.go
@@ -4,11 +4,18 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
 	"wanshantian/grpc/pb"
 )
 
 func main() {
-	conn, _ := grpc.Dial(":1234", grpc.WithInsecure())
+
+	conn, err := grpc.Dial(":1234", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Panic(err)
+	}
+	defer conn.Close()
 	client := pb.NewQueryClient(conn)
 	age, _ := client.GetAge(context.Background(), &pb.UserInfo{Name: "foo"})
 	fmt.Println(age)
