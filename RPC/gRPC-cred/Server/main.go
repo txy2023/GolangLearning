ba0859ab56364dba571f1853dfa2bf62f413ce1a
WanshanTian <wanshantian@gmail.com>
2022-07-01 14:31:20 +0800

    add gRPC-cred

---
+++ RPC/gRPC-cred/Server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"shannont/gRPC-cred/pb"
+)
+
+// 用户信息
+var userinfo = map[string]int32{
+	"foo": 18,
+	"bar": 20,
+}
+
+// Query 结构体，实现QueryServer接口
+// type QueryServer interface {
+//  GetAge(context.Context, *UserInfo) (*AgeInfo, error)
+//	mustEmbedUnimplementedQueryServer()
+// }
+type Query struct {
+	pb.UnimplementedQueryServer // 涉及版本兼容
+}
+
+func (q *Query) GetAge(ctx context.Context, info *pb.UserInfo) (*pb.AgeInfo, error) {
+	age := userinfo[info.GetName()]
+	var res = new(pb.AgeInfo)
+	res.Age = age
+	return res, nil
+}
+
+func main() {
+	// 创建socket监听器
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Panic(err)
+	}
+	// new一个gRPC服务器，用来注册服务
+	grpcserver := grpc.NewServer()
+	// 注册服务方法
+	pb.RegisterQueryServer(grpcserver, new(Query))
+	// 开启gRPC服务
+	err = grpcserver.Serve(listener)
+	if err != nil {
+		log.Panic(err)
+	}
+}
