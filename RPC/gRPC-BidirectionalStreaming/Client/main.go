1a7b73c9f1d53ecc79d4808329cb698314064cd4
WanshanTian <wanshantian@gmail.com>
2022-07-09 18:45:48 +0800

    gRPC-Bidirectional Stream

--- RPC/gRPC-BidirectionalStreaming/Client/main.go
+++ RPC/gRPC-BidirectionalStreaming/Client/main.go
@@ -2,44 +2,52 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"io"
 	"log"
 	"shannont/grpc-bidirectionalStreaming/pb"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	//建立无认证的连接
-	conn, err := grpc.Dial(":1234", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Panic(err)
-	}
+	conn, _ := grpc.Dial(":1234", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 	defer conn.Close()
 	client := pb.NewQueryClient(conn)
+
 	//返回GetAge方法对应的流
+	log.Printf("start of stream")
 	queryStream, _ := client.GetAge(context.Background())
+
 	// 创建goroutine用来向stream中发送message
+	ch := make(chan string, 2)
 	go func() {
-		for i := 0; i < 3; i++ {
-			_ = queryStream.Send(&pb.UserInfo{Name: "foo"})
+		names := []string{"foo", "bar"}
+		for _, v := range names {
+			log.Printf("send message wtih Name is %s\n", v)
+			ch <- v
+			_ = queryStream.Send(&pb.UserInfo{Name: v})
 			time.Sleep(time.Second)
 		}
-		// 调用指定次数后主动关闭流
+		// 主动关闭流
 		err := queryStream.CloseSend()
 		if err != nil {
 			log.Println(err)
 		}
+		close(ch)
 	}()
+
 	// 从stream中接收message
 	for {
+		name := <-ch
 		ageinfoRecv, err := queryStream.Recv()
 		if err == io.EOF {
 			log.Println("end of stream")
 			break
 		}
-		fmt.Println(ageinfoRecv)
+		log.Printf("The age of %s is %d\n", name, ageinfoRecv.GetAge())
 	}
 }
--- RPC/gRPC-BidirectionalStreaming/Server/main.go
+++ RPC/gRPC-BidirectionalStreaming/Server/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"google.golang.org/grpc"
 	"io"
 	"log"
 	"net"
 	"shannont/grpc-bidirectionalStreaming/pb"
+
+	"google.golang.org/grpc"
 )
 
 // 用户信息
@@ -23,16 +24,20 @@ type Query struct {
 	pb.UnimplementedQueryServer // 涉及版本兼容
 }
 
-func (q *Query) GetAge(server pb.Query_GetAgeServer) error {
+func (q *Query) GetAge(ServerStream pb.Query_GetAgeServer) error {
 	log.Println("start of stream")
 	for {
-		userinfoRecv, err := server.Recv()
-		// 带客户端主动关闭流后，退出for循环
+		// 接受message
+		userinfoRecv, err := ServerStream.Recv()
+		// 待客户端主动关闭流后，退出for循环
 		if err == io.EOF {
 			log.Println("end of stream")
 			break
 		}
-		err = server.Send(&pb.AgeInfo{Age: userinfo[userinfoRecv.Name]})
+		log.Printf("The name of user received is %s", userinfoRecv.GetName())
+		// 返回响应message
+		log.Printf("send message about the age of %s", userinfoRecv.GetName())
+		err = ServerStream.Send(&pb.AgeInfo{Age: userinfo[userinfoRecv.Name]})
 		if err != nil {
 			log.Panic(err)
 		}
@@ -42,17 +47,11 @@ func (q *Query) GetAge(server pb.Query_GetAgeServer) error {
 
 func main() {
 	// 创建socket监听器
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		log.Panic(err)
-	}
+	listener, _ := net.Listen("tcp", ":1234")
 	// new一个gRPC服务器，用来注册服务
 	grpcserver := grpc.NewServer()
 	// 注册服务方法
 	pb.RegisterQueryServer(grpcserver, new(Query))
 	// 开启gRPC服务
-	err = grpcserver.Serve(listener)
-	if err != nil {
-		log.Panic(err)
-	}
+	_ = grpcserver.Serve(listener)
 }
