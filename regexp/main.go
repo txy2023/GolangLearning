cc3db16e942b6ecde8cef1bf5938aa22ab1c9d8e
shannont <wanshantian@gmail.com>
2023-08-05 12:28:18 +0800

    regexp

--- regexp/main.go
+++ regexp/main.go
@@ -11,10 +11,12 @@ import (
 func main() {
 	file, _ := os.Open("./hello.txt")
 	contents, _ := ioutil.ReadAll(file)
-	wanted := regexp.MustCompile(`(?m:^hello.*)`)
+	wanted := regexp.MustCompile(`^hello.*`)
 	res := wanted.FindAllString(string(contents), -1)
 	fmt.Println(res)
 	log.Println(res)
 	fmt.Println(len(res))
 	fmt.Println(res[0])
+	a := []string{"111"}
+	fmt.Println(a)
 }
