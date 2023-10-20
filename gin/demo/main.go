940f9908ed376aca5c3cf38347ca4a93f068f447
shannont <wanshantian@gmail.com>
2023-10-21 00:24:28 +0800

    gin

--- gin/demo/main.go
+++ gin/demo/main.go
@@ -4,10 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("for", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "bar",
 		})
 	})
-	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+	r.Run() // 默认监听并在 127.0.0.1:8080 上启动服务
 }
